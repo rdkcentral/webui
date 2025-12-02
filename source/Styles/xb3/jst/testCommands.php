f85e90c98016a13e2e11752dc231732d3021b0f2
Roushan Singh <roushan.s@tataelxsi.co.in>
2025-12-02 15:04:14 +0530

    Adding script changes for webui. Do not merge

--- source/Styles/xb3/jst/testCommands.php
+++
@@ -1,7 +0,0 @@
-<?php
-// This is a separate file on the server (e.g., api_endpoint.php)
-
-// This command runs ONLY when the client-side fetch() function requests this file
-exec("/bin/touch /tmp/Robert1.txt");
-echo "Command executed on the server.";
-?>
